7b9eb1bd8ffad15f90d0c7816ee8a6f2f94db850
Manmeet Singh <manmeet110907@gmail.com>
2026-06-02 08:39:04 +0530

    Added Linear Search implementation

---
+++ searching/LinearSearch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int target = 30;
+    int n = 5;
+
+    bool found = false;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            cout << "Element found at index: " << i << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
